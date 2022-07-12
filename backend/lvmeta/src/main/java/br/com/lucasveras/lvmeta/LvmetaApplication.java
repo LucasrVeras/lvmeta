58d176deed88d86114703242ace3abbe2dee87af
LucasrVeras <lucas.veras01@gmail.com>
2022-07-12 15:19:15 -0300

    Project created

---
+++ backend/lvmeta/src/main/java/br/com/lucasveras/lvmeta/LvmetaApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasveras.lvmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvmetaApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(LvmetaApplication.class, args);
+	}
+
+}
---
+++ backend/lvmeta/src/test/java/br/com/lucasveras/lvmeta/LvmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasveras.lvmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
